a8d4ff910c1298e4f76cdfef8f4da88d0a416689
Maksim Krokhmalyuk <maxim.krokhmaliuk@gmail.com>
2024-11-01 15:00:58 +0600

    Net realized. Choice which proccessor to use is in progress

---
+++ DataLoader.py
@@ -0,0 +1,97 @@
+import time
+
+import matplotlib
+import torch
+import torchvision
+import torch.optim as optim
+import torch.nn.functional as Func
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from Model import CNN
+
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as pyplt
+
+# Hyperparameters
+epochs = 10
+batch_size = 512
+lr = 0.001
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+train_dataset = torchvision.datasets.MNIST("dataset", True, download=True, transform=transforms.ToTensor())
+test_dataset = torchvision.datasets.MNIST("dataset", False, download=True, transform=transforms.ToTensor())
+
+train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, 1, shuffle=True)
+
+model = CNN()
+criterion = nn.CrossEntropyLoss()
+try:
+    model_state_dict = torch.load("parameters/model_state_dict.pt", weights_only=True)
+    model.load_state_dict(model_state_dict)
+    raise EOFError
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+except (FileNotFoundError, EOFError):
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    for epoch in range(epochs):
+        print(epoch)
+        for images, labels in train_dataloader:
+            logits = model(images)  # forward pass
+            loss = criterion(logits, labels)
+
+            optimizer.zero_grad()
+            model.zero_grad() # not required
+            # через те що за замовчуванням попередні градієнти додаються до поточних при обчисленні
+            # optimizer.zero_grad() обнуляє також і градієнти моделі
+
+            loss.backward()
+            optimizer.step()  # update weights
+    torch.save(model.state_dict(), "parameters/model_state_dict.pt")
+    # model state dict зберігає параметри для моделі (weights, biases)
+    # та параметри для batch norm (dispersion, average, batches counter)
+
+model.eval()
+torch.set_grad_enabled(False)
+x = []
+y = []
+for idx, (img, label) in enumerate(test_dataloader):
+    logits = Func.softmax(model.forward(img))
+    # print(label.item())
+    # print(logits)
+    # print(logits.shape)
+    # print(logits[0, label.item()].item())
+    # print()
+    res = (1.0 - logits[0, label.item()].item()) * 100
+    # if res > 50.0:
+    #     image_np = img.numpy().squeeze()  # Використовуйте squeeze() для видалення зайвих вимірів
+    #
+    #     # Виведення зображення
+    #     pyplt.imshow(image_np, cmap='gray')  # Відображаємо зображення у відтінках сірого
+    #     pyplt.title(f'Label: {label}')  # Додаємо заголовок із міткою
+    #     pyplt.axis('off')  # Вимикаємо осі
+    #     print(f"logits - {logits}\nlabel - {label}")
+    #     pyplt.show()  # Показуємо графік
+    #     break
+    # # print(res)
+    x.append(idx)
+    y.append(res)
+# time.sleep(100)
+torch.set_grad_enabled(True)
+
+pyplt.scatter(x, y, label="Похибка в %")
+pyplt.ylim(0, 100)
+pyplt.xlabel("index")
+pyplt.ylabel("loss")
+pyplt.show()
+# state_dict - словник який по шарам зберігає навчальні параметри
+# model.state_dict(), optimizer.state_dict()...
+# model.train() - переводить модель в режим тренування (якщо використ. dropout, batchnorm... вкл)
+# model.eval() - переводить модель в режим оцінювання (якщо використ. dropout, batchnorm... викл)
+# torch.no_grad() - авдключає автоматичне обчислення градієнтів для ефективності (під час тесту)
---
+++ Model.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as net
+import torch.nn.functional as Func
+
+
+class CNN(net.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        dropout_prob = 0.4 # ймовірність виключення нейрона
+        # padding = 1, because 1 pixel is added to every dimension
+        self.conv1_1 = net.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.batch_norm1_1 = net.BatchNorm2d(32)  # глибина вхідного шару
+        self.conv1_2 = net.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.batch_norm1_2 = net.BatchNorm2d(64)
+
+        self.max_pool_1 = net.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout_1 = net.Dropout2d(dropout_prob)
+
+        self.conv2_1 = net.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.batch_norm2_1 = net.BatchNorm2d(64)
+
+        self.max_pool_2 = net.MaxPool2d(kernel_size=2, stride=2)  # dont need 2 max pooling
+        self.dropout_2 = net.Dropout(dropout_prob)
+
+        self.mlp1 = net.Linear(7 * 7 * 64, 256)
+        self.batch_norm_mlp_1 = net.BatchNorm1d(256)
+        self.dropout_mlp_1 = net.Dropout1d(dropout_prob)
+
+        self.mlp2 = net.Linear(256, 128)
+        self.batch_norm_mlp_2 = net.BatchNorm1d(128)
+
+        self.mlp3 = net.Linear(128, 10)
+
+    def forward(self, tensor):
+        # на вході повинен бути 4D тензор для батч норм навіть при тестуванні
+        tensor = Func.relu(self.batch_norm1_1(self.conv1_1(tensor)))
+        tensor = Func.relu(self.batch_norm1_2(self.conv1_2(tensor)))
+        tensor = self.dropout_1(self.max_pool_1(tensor))
+        tensor = Func.relu(self.batch_norm2_1(self.conv2_1(tensor)))
+        tensor = self.dropout_2(self.max_pool_2(tensor))
+        vector = torch.reshape(tensor, (-1, 7 * 7 * 64))
+        vector = Func.relu(self.dropout_mlp_1(self.batch_norm_mlp_1(self.mlp1(vector))))
+        vector = Func.relu(self.batch_norm_mlp_2(self.mlp2(vector)))
+        vector = self.mlp3(vector)  # logits
+        return vector
+
+
+
+
+
